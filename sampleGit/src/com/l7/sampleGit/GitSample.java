a1c9c79ada60e9c132b0cf920ed38e2c74ebd1ba
arathy <Arathy.Menon@COK-A-00028.litmus7.local>
2020-01-14 16:07:15 +0530

    hii added

--- sampleGit/src/com/l7/sampleGit/GitSample.java
+++ sampleGit/src/com/l7/sampleGit/GitSample.java
@@ -3,7 +3,7 @@ package com.l7.sampleGit;
 public class GitSample {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world and hiii");
 
 	}
 
